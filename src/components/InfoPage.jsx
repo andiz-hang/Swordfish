5900911c9acb2220b0a1b64cab8fb7c1c6a0d2a5
Andi Z <andiz@Andis-MacBook-Pro.local>
2023-02-02 15:04:46 -0800

    changed structure of infopage files

--- src/components/InfoPage.jsx
+++ src/components/InfoPage.jsx
@@ -1,24 +1,21 @@
 import "./InfoPage.css";
 import React, { Component } from "react";
 
+import Home from "./info-page-components/Home.jsx";
+import StockInfo from "./info-page-components/StockInfo.jsx";
+
 class InfoPage extends Component {
   // state = {  }
   render() {
     if (this.props.stock === "") {
-      return (
-        <main className="InfoPage">
-          <h1>Welcome to the Site!</h1>
-        </main>
-      );
+      return <Home />;
     }
 
     return (
-      <main className="InfoPage">
-        <h1>Current Stock: {this.props.stock}</h1>
-        <button onClick={() => this.props.onAddStock(this.props.stock)}>
-          Add Stock to List
-        </button>
-      </main>
+      <StockInfo
+        onAddStock={this.props.onAddStock()}
+        stock={this.props.stock}
+      />
     );
   }
 }
