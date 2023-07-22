839a7d320653acb8e23947676cd64b854d7cf4cb
Andi Z <andiz@Andis-MacBook-Pro.local>
2023-07-21 17:27:02 -0700

    clean up code

--- src/components/info-page-components/charts.js
+++ src/components/info-page-components/charts.js
@@ -4,29 +4,9 @@ import { Bar, Line } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 import { Chart } from "react-chartjs-2";
 
-export const TestChart = () => {
-  return (
-    <Line
-      // datasetIdKey='id'
-      data={{
-        labels: ["2022","2021","2020","2019","2018"],
-        datasets: [
-          {
-            id: 1,
-            label: '',
-            data: [111443,92953,73365,58896,64121],
-          },
-        ],
-      }}
-    />
-  );
-};
-
 export const LineChart = (props) => {
-
   return (
     <Line
-      // datasetIdKey='id'
       data={props.chartData}
       options={props.options}
     />
@@ -34,14 +14,29 @@ export const LineChart = (props) => {
 };
 
 export const BarChart = (props) => {
-
-  // console.log("BarChart", props.chartData);
-
   return (
     <Bar
-      // datasetIdKey={props.id}
       data={props.chartData}
       options={props.options}
     />
   );
 };
+
+// CHART FOR TESTING PURPOSES ONLY
+// export const TestChart = () => {
+//   return (
+//     <Line
+//       // datasetIdKey='id'
+//       data={{
+//         labels: ["2022","2021","2020","2019","2018"],
+//         datasets: [
+//           {
+//             id: 1,
+//             label: '',
+//             data: [111443,92953,73365,58896,64121],
+//           },
+//         ],
+//       }}
+//     />
+//   );
+// };
