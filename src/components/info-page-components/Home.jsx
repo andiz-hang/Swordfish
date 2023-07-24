3342e8cc147d17e313611ee4ed21a8361d57c33c
Andi Z <andiz@Andis-MacBook-Pro.local>
2023-07-24 15:52:28 -0700

    added some info on the Home page

--- src/components/info-page-components/Home.jsx
+++ src/components/info-page-components/Home.jsx
@@ -6,6 +6,22 @@ class Home extends Component {
     return (
       <main className="InfoPage">
         <h1>Welcome to the Site!</h1>
+
+        <h2>Tips:</h2>
+        <h2>
+          The app will search for companies listed on US exchanges by default.
+          To search for companies listed on other exchanges, add the country
+          code after the ticker symbol in the search bar
+        </h2>
+        <h2>The supported countries are:</h2>
+        <ul>
+          <li>Canada (:CA)</li>
+          <li>Australia (:AU)</li>
+          <li>New Zealand (:NZ)</li>
+          <li>Mexico (:MM)</li>
+          <li>United Kingdom (:LN)</li>
+        </ul>
+        <h3>Examples: "T:CA", "GFNORTEO:MM"</h3>
       </main>
     );
   }
