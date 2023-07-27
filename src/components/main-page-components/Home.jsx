69070664d2683912f7b6c47a16d429a8603d4614
Andi Z <andiz@Andis-MacBook-Pro.local>
2023-07-27 12:29:01 -0700

    minor formattings

--- src/components/main-page-components/Home.jsx
+++ src/components/main-page-components/Home.jsx
@@ -8,11 +8,11 @@ class Home extends Component {
         <h1>Welcome to the Site!</h1>
 
         <h2>Tips:</h2>
-        <h2>
+        <p>
           The app will search for companies listed on US exchanges by default.
           To search for companies listed on other exchanges, add the country
           code after the ticker symbol in the search bar
-        </h2>
+        </p>
         <h2>The supported countries are:</h2>
         <ul>
           <li>Canada (:CA)</li>
@@ -21,7 +21,7 @@ class Home extends Component {
           <li>Mexico (:MM)</li>
           <li>United Kingdom (:LN)</li>
         </ul>
-        <h3>Examples: "T:CA", "GFNORTEO:MM"</h3>
+        <p>Examples: "T:CA", "GFNORTEO:MM"</p>
       </main>
     );
   }
