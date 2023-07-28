fd5f8fe7c92318fd72234345ace0ac33878cc440
Andi Z <andiz@Andis-MacBook-Pro.local>
2023-07-28 16:02:45 -0700

    added info section

--- src/components/main-page-components/Home.jsx
+++ src/components/main-page-components/Home.jsx
@@ -6,22 +6,6 @@ class Home extends Component {
     return (
       <main className="MainPage">
         <h1>Welcome to the Site!</h1>
-
-        <h2>Tips:</h2>
-        <p>
-          The app will search for companies listed on US exchanges by default.
-          To search for companies listed on other exchanges, add the country
-          code after the ticker symbol in the search bar
-        </p>
-        <h2>The supported countries are:</h2>
-        <ul>
-          <li>Canada (:CA)</li>
-          <li>Australia (:AU)</li>
-          <li>New Zealand (:NZ)</li>
-          <li>Mexico (:MM)</li>
-          <li>United Kingdom (:LN)</li>
-        </ul>
-        <p>Examples: "T:CA", "GFNORTEO:MM"</p>
       </main>
     );
   }
--- src/components/main-page-components/Info.jsx
+++ src/components/main-page-components/Info.jsx
@@ -6,6 +6,29 @@ class Info extends Component {
     return (
       <main className="MainPage">
         <h1>This is the Info Page</h1>
+
+        <h1>Please read before proceeding!</h1>
+        <ul>
+          <li>Bank/Insurance Stocks</li>
+          <li>Api Daily Limit</li>
+          <li>Stock list refresh</li>
+          <li>
+            <p>
+              The app will search for companies listed on US exchanges by
+              default. To search for companies listed on other exchanges, add
+              the country code after the ticker symbol in the search bar
+            </p>
+            <p>The supported countries are:</p>
+            <ul>
+              <li>Canada (:CA)</li>
+              <li>Australia (:AU)</li>
+              <li>New Zealand (:NZ)</li>
+              <li>Mexico (:MM)</li>
+              <li>United Kingdom (:LN)</li>
+            </ul>
+            <p>Examples: "T:CA", "GFNORTEO:MM"</p>
+          </li>
+        </ul>
       </main>
     );
   }
